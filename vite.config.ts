b934c754c0d9b01d9af90f9916d90fc4a17a4736
gabrielefasanoo <gabrielefasano13@gmail.com>
2025-03-17 18:14:28 +0100

    prova modifica package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-icons/bs']
   },
